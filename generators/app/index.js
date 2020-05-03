922d3cf5e576f13e535168bf63d5f963bcdc3737
Ewan Harrow <ewan@harrow.org>
2020-05-03 13:27:42 +0100

    feat: convert post title to title case and convert tags to space separated and kebab where necessary

--- generators/app/index.js
+++ generators/app/index.js
@@ -43,22 +43,40 @@ module.exports = class extends Generator {
 
   writing() {
     // this.log("post title", this.props.title);
-
+    const CSVToArray = (data, delimiter = ',', omitFirstRow = false) =>
+      data
+        .slice(omitFirstRow ? data.indexOf('\n') + 1 : 0)
+        .split('\n')
+        .map(v => v.split(delimiter));
+    
     const toKebabCase = str =>
       str &&
       str
         .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
         .map(x => x.toLowerCase())
         .join("-");
+
+    const CSVToString = (acc, item) => {
+      return `${acc} ${item}`
+    }
+
+    const toTitleCase = str =>
+      str
+        .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+        .map(x => x.charAt(0).toUpperCase() + x.slice(1))
+        .join(' ');
+
+    const kebabTags = CSVToArray(this.props.tags)[0].map((item) => toKebabCase(item)).reduce(CSVToString);
     const kebabDate = moment(new Date()).format("YYYY-MM-DD");
     const kebabPostTitle = toKebabCase(this.props.title);
+    const title = toTitleCase(this.props.title);
 
     this.fs.copyTpl(
       this.templatePath("template-post.md"),
       this.destinationPath(`${kebabDate}-${kebabPostTitle}.md`),
       {
-        title: this.props.title,
-        tags: this.props.tags,
+        title: title,
+        tags: kebabTags,
         comments: this.props.comments
       }
     );
